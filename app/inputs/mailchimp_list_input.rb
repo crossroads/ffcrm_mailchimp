f6cf6144d5244b968075392e9bccd23c60bb5a3b
Steve Kenworthy <steveyken@gmail.com>
2013-12-12 14:04:46 +0800

    Back to storing list and group choices as an Array as this helps with checkboxes. Completed UI and model work.

--- app/inputs/mailchimp_list_input.rb
+++ app/inputs/mailchimp_list_input.rb
@@ -5,46 +5,40 @@ class MailchimpListInput < SimpleForm::Inputs::CollectionCheckBoxesInput
   # Generate a checkbox list of groups
   #------------------------------------------------------------------------------
   def input
-
     out = "<br/>".html_safe
 
-    out << "<input id='#{attribute_name}[list_id]' name='#{attribute_name}[list_id]' type='checkbox' value='#{cf.list_id}'>".html_safe
-    out << "#{cf.list.name} (subscribe to entire list)<br /><dd><ul>".html_safe
+    cf_klass_name = cf.klass.to_s
+    id = "#{cf_klass_name.underscore}_#{attribute_name}_list_#{cf.list_id}"
+    name = "#{cf_klass_name.underscore}[#{attribute_name}][]"
+    out << "<input id='#{id}' name='#{name}' type='checkbox' value='list_#{cf.list_id}' #{list_checked? ? 'checked=\'checked\'' : ''} >".html_safe
+    out << "<label for='#{id}'>#{cf.list.name} (subscribe to entire list)</label><br /><dd><ul>".html_safe
 
     out << @builder.collection_check_boxes(attribute_name, collection, :name, :name, input_options, input_html_options) do |b|
-      name = "#{attribute_name}[groups][#{b.object.name}]"
-      id = name.underscore
-      '<li>'.html_safe + b.check_box( id: id, name: name ) + b.label + '</li>'.html_safe
+      '<li>'.html_safe + b.check_box + b.label + '</li>'.html_safe
     end
 
     out << '</ul></dd>'.html_safe
-
     out
-
   end
 
   private
 
-  # SimpleForm uses this to populate the checkboxes
+  # SimpleForm uses this to populate the checkboxes for the groups
   # [ ['id', 'name'], ['id', 'name'] ]
   #------------------------------------------------------------------------------
   def collection
     cf.list.groups
   end
 
-  # Pre-check group ids that have been previously saved
-  def input_options
-    super.merge( { checked: value } )
-  end
-
-  # selected group ids
-  def value
-    object.send(attribute_name)["groups"] || []
-  end
-
   # Provides a reference to the custom field instance
   def cf
     @cf ||= CustomFieldMailchimpList.find_by_name(attribute_name)
+  end
+
+  #
+  # true if the list itself is already checked
+  def list_checked?
+    object.send("#{attribute_name}_list").any?
   end
 
 end
--- app/models/fields/custom_field_mailchimp_list.rb
+++ app/models/fields/custom_field_mailchimp_list.rb
@@ -36,7 +36,7 @@ class CustomFieldMailchimpList < CustomField
     settings['list_id'] = value
   end
 
-  # Serialize mailchimp lists as Hash
+  # Serialize mailchimp lists as Array
   #------------------------------------------------------------------------------
   def apply_serialization
     klass_name = self.field_group.try(:klass_name)
@@ -44,22 +44,43 @@ class CustomFieldMailchimpList < CustomField
     klass = klass_name.constantize
 
     if !klass.serialized_attributes.keys.include?(self.name)
-      klass.serialize(self.name.to_sym, Hash)
+      klass.serialize(self.name.to_sym, Array)
       Rails.logger.debug("FfcrmMailchimp: Serializing #{self.name} as Array for #{klass}.")
     end
 
     #
-    # Override the mutator on the object class to ensure items are serialized correctly
+    # We store the list and group data as an Array on the custom field
+    # E.g. ["group1", "group2", "list_1235432"]
+    #
+    # We define methods on the klass related to the custom field so, for example,
+    # if a custom field is named 'cf_newsletter' then the following methods are defined:
+    # contact.cf_newsletter=       - takes checkbox input and serializes into an array
+    # contact.cf_newsletter_list   - returns the list if it is checked
+    # contact.cf_newsletter_groups - returns the groups that have been checked
     attr = self.name
     unless klass.instance_methods.include?(:"#{attr}=")
       klass.class_eval <<-WRITER, __FILE__, __LINE__ + 1
-        define_method "list_with_group" do |val|
-          {"list_id" => "#{list_id}", "groups" => val} if val.present?
-        end
+
+        # Override the mutator on the object class to ensure items are serialized correctly
+        # ["", "group1,group2,list_1235432"] becomes ["group1", "group2", "list_1235432"]
         define_method "#{attr}=" do |value|
-          write_attribute( attr, list_with_group(value.reject(&:blank?)) )
+          write_attribute( attr, value.join(',').split(',').reject(&:blank?) )
         end
+
+        # Return the list if it is checked
+        # ["group1", "group2", "list_1235432"] returns ["list_1235432"] or []
+        define_method "#{attr}_list" do
+          read_attribute(attr).select{|v| v.starts_with?('list_')}.compact
+        end
+
+        # Return the groups that are checked
+        # ["group1", "group2", "list_1235432"] returns ["group1", "group2"]
+        define_method "#{attr}_groups" do
+          read_attribute(attr).reject{|v| v.starts_with?('list_')}.compact
+        end
+
       WRITER
     end
+
   end
 end
