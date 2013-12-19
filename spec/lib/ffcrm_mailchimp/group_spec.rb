c770bab83613509565221b4e9950733cca89b535
Steve Kenworthy <steveyken@gmail.com>
2013-12-19 12:41:04 +0800

    Removed debugger statements

--- spec/lib/ffcrm_mailchimp/group_spec.rb
+++ spec/lib/ffcrm_mailchimp/group_spec.rb
@@ -6,14 +6,12 @@ describe FfcrmMailchimp::Group do
   let(:interest_groupings){ FactoryGirl.build_list(:interest_groupings, 4)}
 
   before(:each) do
-    debugger
     Gibbon::API.any_instance.stub_chain(:lists, :interest_groupings).and_return([{"groups" => interest_groupings}])
   end
 
   describe ".groups_for" do
     context "mailchimp account" do
       it "should return all the lists" do
-        debugger
         FfcrmMailchimp::Group.groups_for("test1234").should_not be_blank
       end
     end
