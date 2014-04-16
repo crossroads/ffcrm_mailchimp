fd09a802c7064e44dcfe22cd58c5696324872efa
Steve Kenworthy <steveyken@gmail.com>
2014-04-16 12:38:02 +0800

    Added check for list with groups

--- lib/ffcrm_mailchimp/subgroup_validation.rb
+++ lib/ffcrm_mailchimp/subgroup_validation.rb
@@ -5,13 +5,13 @@ ActiveSupport.on_load(:fat_free_crm_contact) do
 
   class_eval do
 
-    # If subgroup_only is checked, ensures list cannot be saved without at least one subgroup selected.
+    # If subgroup_only is checked, ensures list cannot be saved without at least one subgroup selected (if groups exist on the list)
     # ---------------------------------------------------------
     validate do
       if FfcrmMailchimp.config.subgroups_only?
         FfcrmMailchimp.config.mailchimp_list_fields.each do |field|
           subscription = FfcrmMailchimp::ListSubscription.new( self.send(field.name) )
-          if subscription.has_list? and !subscription.has_at_least_one_group?
+          if subscription.has_list? and !subscription.has_at_least_one_group? and field.groups.any?
             self.errors.add(:base, "You must either select some mailing list groups for '#{field.label}' or uncheck the box to remove them from the list.")
           end
         end
